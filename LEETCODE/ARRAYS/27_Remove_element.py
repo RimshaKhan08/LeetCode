a70eac6b803ee84c5fa67baf5bd6a115fb2bb2b2
RimshaKhan08 <getrimshakhan@gmail.com>
2025-09-22 23:51:31 +0500

    Add files via upload

---
+++ LEETCODE/ARRAYS/27_Remove_element.py
@@ -0,0 +1,11 @@
+def removeElement(nums, val):
+        i = 0
+        while i < len(nums):
+           if nums[i] == val:
+              nums.pop(i)
+           else:
+              i += 1
+        return len(nums)
+        
+nums = [3,2,3,2,2]
+print(removeElement(nums, 2))
